dbfa6d60ce8afe121dc58a54070df77675f5c4ae
Mathias Emil Blad Larsen <mebl94@gmail.com>
2017-02-20 10:10:08 +0100

    Har testet move(left).

--- GameLaunch.java
+++ GameLaunch.java
@@ -2,6 +2,9 @@ public class GameLaunch
 {
     public static void main(String[] args)
     {
+        GamePiece snake = new GamePiece(3, 10, "Haha I'm hungry.", "I'm standing on square", "snake");
+        snake.move("left");
+        
         
     }
 }
