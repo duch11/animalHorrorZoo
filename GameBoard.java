2a2cec929834bdf6b7e94cab43368a3a7f9140c6
Andreas <rednex702@hotmail.com>
2017-02-22 14:57:18 +0100

    movecommands

--- GameBoard.java
+++ GameBoard.java
@@ -5,7 +5,24 @@ public class GameBoard
    public void move(GamePiece piece, String direction)
    {
         piece.setPos(1,2);
-        System.out.println("Hej fra move" + piece.getPosY() );
+        System.out.println(piece.getPosX() + " " + piece.getPosY() );
+        
+        if (direction.equals("left"))
+        {
+           System.out.println("Moving left"); 
+        }
+        if (direction.equals("right"))
+        {
+           System.out.println("Moving right"); 
+        }
+        if (direction.equals("up"))
+        {
+           System.out.println("Moving up"); 
+        }
+        if (direction.equals("down"))
+        {
+           System.out.println("Moving down"); 
+        }
         
    } 
 
